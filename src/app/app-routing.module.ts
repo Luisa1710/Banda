ef55d780f529616409dc630e8734488bec91ee5d
Luisa Ospina <ospiluisa1710@gmail.com>
2022-09-16 11:25:22 -0500

    banda

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumsComponent } from './albums/albums.component';
+import { HistoriaComponent } from './historia/historia.component';
+import { HomeComponent } from './home/home.component';
+import { IntegrantesComponent } from './integrantes/integrantes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent,pathMatch:"full"},
+  {path:"historia",component:HistoriaComponent},
+  {path:"integrantes",component:IntegrantesComponent},
+  {path:"albums",component:AlbumsComponent}
+
+]; 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
